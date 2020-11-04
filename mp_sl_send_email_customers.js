70fbe55daf88045a9d338f135e686ec8cb69fe51
Ankith Ravindran <ankith88@gmail.com>
2020-11-04 11:40:43 +1100

    Update - Ankith

---
+++ mp_sl_send_email_customers.js
@@ -0,0 +1,216 @@
+/**
+ * Module Description
+ * 
+ * NSVersion    Date            			Author         
+ * 1.00       	2020-07-20 11:05:54   		Ankith
+ *
+ * Description:         
+ * 
+ * @Last Modified by:   Ankith
+ * @Last Modified time: 2020-07-22 10:50:05
+ *
+ */
+
+var baseURL = 'https://1048144.app.netsuite.com';
+if (nlapiGetContext().getEnvironment() == "SANDBOX") {
+    baseURL = 'https://system.sandbox.netsuite.com';
+}
+
+var zee = 0;
+var role = nlapiGetRole();
+
+if (role == 1000) {
+    zee = nlapiGetUser();
+} else if (role == 3) { //Administrator
+    zee = 6; //test
+} else if (role == 1032) { // System Support
+    zee = 425904; //test-AR
+}
+
+function sendEmail(request, response) {
+
+    if (request.getMethod() === "GET") {
+        var form = nlapiCreateForm('Mass Email to Customers');
+
+        var letter_type = request.getParameter('type');
+
+        var inlinehTML = '';
+
+        var inlinehtml2 = '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script><script src="//code.jquery.com/jquery-1.11.0.min.js"></script><link rel="stylesheet" type="text/css" href="https://1048144.app.netsuite.com/core/media/media.nl?id=2392606&c=1048144&h=a4ffdb532b0447664a84&_xt=.css"/><script type="text/javascript"  src="https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.js"></script><script src="https://cdn.datatables.net/fixedheader/3.1.2/js/dataTables.fixedHeader.min.js" type="text/javascript"></script><link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/fixedheader/3.1.2/css/fixedHeader.dataTables.min.css"><link href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet"><script src="//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script><link rel="stylesheet" href="https://1048144.app.netsuite.com/core/media/media.nl?id=2060796&c=1048144&h=9ee6accfd476c9cae718&_xt=.css"/><script src="https://1048144.app.netsuite.com/core/media/media.nl?id=2060797&c=1048144&h=ef2cda20731d146b5e98&_xt=.js"></script><link type="text/css" rel="stylesheet" href="https://1048144.app.netsuite.com/core/media/media.nl?id=2090583&c=1048144&h=a0ef6ac4e28f91203dfe&_xt=.css"><link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.css" rel="stylesheet"><script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.5/jspdf.debug.js" integrity="sha384-CchuzHs077vGtfhGYl9Qtc7Vx64rXBXdIAZIPbItbNyWIRTdG0oYAqki3Ry13Yzu" crossorigin="anonymous"></script><style>.mandatory{color:red;}ol {margin: 0 0 1.5em;padding: 0;counter-reset: item;}ol > li {margin: 0;padding: 0 0 0 2em;text-indent: -2em;list-style-type: none;counter-increment: item;}ol > li:before {display: inline-block;width: 1em;padding-right: 0.5em;font-weight: bold;text-align: right;content: counter(item) ".";}</style>';
+
+
+        //Searcf for Campaign Templates
+        var searchedCampTemp = nlapiLoadSearch('customrecord_camp_comm_template', 'customsearch_salesp_campaign_templates');
+
+
+        var newFiltersCampTemp = new Array();
+
+        newFiltersCampTemp[newFiltersCampTemp.length] = new nlobjSearchFilter('custrecord_camp_comm_camp_type', null, 'anyof', 2);
+        newFiltersCampTemp[newFiltersCampTemp.length] = new nlobjSearchFilter('isinactive', null, 'is', 'F');
+        newFiltersCampTemp[newFiltersCampTemp.length] = new nlobjSearchFilter('custrecord_camp_comm_comm_type', null, 'anyof', 1);
+
+
+        searchedCampTemp.addFilters(newFiltersCampTemp);
+
+        var resultSetCampTemp = searchedCampTemp.runSearch();
+
+        inlinehtml2 += email_template(resultSetCampTemp);
+
+
+        if (role != 1000) {
+
+            inlinehtml2 += '<div class="col-xs-4 admin_section" style="width: 20%;left: 40%;position: absolute;"><b>Select Zee</b> <select class="form-control zee_dropdown" >';
+
+            //WS Edit: Updated Search to SMC Franchisee (exc Old/Inactives)
+            //Search: SMC - Franchisees
+            var searched_zee = nlapiLoadSearch('partner', 'customsearch_smc_franchisee');
+
+            var resultSet_zee = searched_zee.runSearch();
+
+            var count_zee = 0;
+
+            var zee_id;
+
+            inlinehtml2 += '<option value=""></option>'
+
+            resultSet_zee.forEachResult(function(searchResult_zee) {
+                zee_id = searchResult_zee.getValue('internalid');
+                // WS Edit: Updated entityid to companyname
+                zee_name = searchResult_zee.getValue('companyname');
+
+                if (request.getParameter('zee') == zee_id) {
+                    inlinehtml2 += '<option value="' + zee_id + '" selected="selected">' + zee_name + '</option>';
+                } else {
+                    inlinehtml2 += '<option value="' + zee_id + '">' + zee_name + '</option>';
+                }
+
+                return true;
+            });
+
+            inlinehtml2 += '</select></div>';
+        }
+
+        if (!isNullorEmpty(request.getParameter('zee'))) {
+            zee = request.getParameter('zee');
+        }
+
+        inlinehtml2 += '<br><br><table border="0" cellpadding="15" id="customer" class="display tablesorter table table-striped table-bordered table-responsive" cellspacing="0"><thead style="color: white;background-color: #607799;"><tr><th class="text-center">ID</th><th class="text-center">Customer</th><th class="text-center">5Kg </th><th class="text-center">3Kg</th><th  class="text-center">1Kg</th><th class="text-center">500g</th><th class="text-center">B4</th><th class="text-center">C5</th><th class="text-center">DL</th></tr></thead>';
+
+
+
+        /**
+         * Description - Get the list of Customer that have Trial or Signed Status for a particular zee
+         */
+
+
+        // inlineQty += '</tbody>';
+        inlinehtml2 += '</table><br/>';
+
+        form.addField('custpage_letter_type', 'textarea', 'BODY').setDisplayType('hidden').setDefaultValue(letter_type);
+        form.addField('custpage_template', 'textarea', 'BODY').setDisplayType('hidden');
+        form.addField('custpage_subject', 'textarea', 'BODY').setDisplayType('hidden');
+        form.addField('custpage_body', 'textarea', 'BODY').setDisplayType('hidden');
+        form.addField('custpage_pdf', 'textarea', 'BODY').setDisplayType('hidden');
+        form.addField('custpage_result_set_length', 'textarea', 'BODY').setDisplayType('hidden');
+
+        form.addField('preview_table', 'inlinehtml', '').setLayoutType('outsidebelow', 'startrow').setDefaultValue(inlinehtml2);
+        form.setScript('customscript_cl_mass_email_customers');
+        // form.addButton('pdfprint', 'Print Letters', 'onclick_print()');
+        form.addSubmitButton('Send Email');
+
+        response.writePage(form);
+    } else {
+        var letter_type = request.getParameter('custpage_letter_type');
+        var subject = request.getParameter('custpage_subject');
+        var message = request.getParameter('custpage_body');
+        var template = request.getParameter('custpage_template');
+        var pdf = request.getParameter('custpage_pdf');
+        var result_set = request.getParameter('custpage_result_set_length');
+
+        var params = {
+            custscript_letter_type: letter_type,
+            custscript_subject: subject,
+            custscript_template: template,
+            custscript_pdf: pdf
+        };
+        var reschedule_status = nlapiScheduleScript('customscript_ss_mass_email_customers', 'customdeploy1', params);
+        nlapiLogExecution('DEBUG', 'reschedule_status', reschedule_status);
+
+        var params_progress = {
+            custparam_letter_type: letter_type,
+            custparam_subject: subject,
+            custparam_template: template,
+            custparam_pdf: pdf,
+            custparam_result_set_length: result_set
+        };
+        nlapiSetRedirectURL('SUITELET', 'customscript_sl_sent_email', 'customdeploy1', null, params_progress);
+    }
+}
+
+/**
+ * [email_template description]
+ * @param  {[campaign types from a search]} resultSetCampTemp [description]
+ * @return {[string]}                   	html			  [description]
+ */
+function email_template(resultSetCampTemp) {
+    var html = '<div class="form-group container row_to ">';
+    html += '<div class="row">'
+
+    html += '<div class="form-group container row_template">';
+    html += '<div class="row">'
+
+    html += '<div class="col-xs-8 template_section hide"><div class="input-group"><span class="input-group-addon">TEMPLATE';
+
+    html += '<span class="mandatory">*</span>';
+
+    html += '</span><select  id="template" class="form-control" ><option></option>';
+    resultSetCampTemp.forEachResult(function(searchResultCampTemp) {
+
+        var tempId = searchResultCampTemp.getValue('internalid');
+        var tempName = searchResultCampTemp.getValue('name');
+        var email_template_id = searchResultCampTemp.getValue('custrecord_camp_comm_email_template');
+
+        html += '<option value="' + tempId + '">' + tempName + '</option>'
+
+
+
+        return true;
+    });
+    html += '</select></div></div>';
+    html += '</div>';
+    html += '</div>';
+
+
+    html += '<div class="form-group container row_subject hide">';
+    html += '<div class="row">'
+    html += '<div class="col-xs-8 subject_section"><div class="input-group"><span class="input-group-addon">SUBJECT ';
+    html += '</span><input type="text" id="subject" class="form-control" /></div></div>';
+    html += '</div>';
+    html += '</div>';
+
+    html += '<div class="form-group container row_body hide">';
+    html += '<div class="row">'
+    html += '<div class="col-xs-12 body_section"><div id="email_body"></div></div>';
+    html += '</div>';
+    html += '</div>';
+    html += '<div class="form-group container row_body ">';
+    html += '<div class="row">'
+    html += '<div class="col-xs-12 body_section"><textarea id="preview" class="hide"></textarea><iframe id="viewer" frameborder="0" scrolling="no" width="400" height="600" style="display: none;"></iframe></div>';
+    html += '</div>';
+    html += '</div>';
+    return html;
+}
+
+/**
+ * [Todays Date]
+ * @return {[type]} [description]
+ */
+function getDate() {
+    var date = new Date();
+    if (date.getHours() > 6) {
+        date = nlapiAddDays(date, 1);
+    }
+    date = nlapiDateToString(date);
+
+    return date;
+}
---
+++ mp_ss_send_email_customers.js
@@ -0,0 +1,106 @@
+/**
+ * Module Description
+ * 
+ * NSVersion    Date            			Author         
+ * 1.00       	2020-07-20 18:06:05   		Ankith
+ *
+ * Description:         
+ * 
+ * @Last Modified by:   Ankith Ravindran
+ * @Last Modified time: 2020-11-04 10:27:13
+ *
+ */
+
+var adhoc_inv_deploy = 'customdeploy2';
+var prev_inv_deploy = null;
+var ctx = nlapiGetContext();
+
+function sendEmailSS() {
+
+    var zee = ctx.getSetting('SCRIPT', 'custscript_mass_email_zee');
+    var subject = ctx.getSetting('SCRIPT', 'custscript_mass_email_subject');
+    var template = ctx.getSetting('SCRIPT', 'custscript_mass_email_template');
+
+    prev_inv_deploy = ctx.getDeploymentId();
+
+
+
+    var customerSearch = nlapiLoadSearch('customer', 'customsearch_mass_email_customer_list');
+
+    var addFilterExpression = new nlobjSearchFilter('partner', null, 'anyof', zee);
+    customerSearch.addFilter(addFilterExpression);
+    var resultSetCustomer = customerSearch.runSearch();
+
+    var all_pages = [];
+
+    resultSetCustomer.forEachResult(function(searchResult) {
+
+        var usage_loopstart_cust = ctx.getRemainingUsage();
+        if ((usage_loopstart_cust < 400)) {
+            var params = {
+                custscript_letter_zee: zee,
+                custscript_subject: subject,
+                custscript_template: template
+            }
+
+            reschedule = rescheduleScript(prev_inv_deploy, adhoc_inv_deploy, params);
+            nlapiLogExecution('AUDIT', 'Reschedule Return', reschedule);
+            if (reschedule == false) {
+                return false;
+            }
+        }
+
+        var custid = searchResult.getValue('internalid');
+        var entityid = searchResult.getValue('entityid');
+        var companyname = searchResult.getValue('companyname');
+
+        var recCustomer = nlapiLoadRecord('customer', custId);
+        var account_email = recCustomer.getFieldValue('email');
+        var service_email = recCustomer.getFieldValue('custentity_email_service');
+
+        recCustomer.setFieldValue('custentity_mass_email_sent', 1);
+
+
+        var emailAttach = new Object();
+        emailAttach['entity'] = custId;
+
+        if (isNullorEmpty(account_email) && isNullorEmpty(service_email)) {
+
+        } else {
+
+            if (!isNullorEmpty(account_email)) {
+                nlapiSendEmail(112209, account_email, subject, emailBody, null, null, emailAttach, null, true);
+            } else if (!isNullorEmpty(service_email)) {
+                nlapiSendEmail(112209, service_email, subject, emailBody, null, null, emailAttach, null, true);
+            }
+        }
+
+
+        nlapiSubmitRecord(recCustomer);
+
+        var params = {
+            custscript_letter_zee: zee,
+            custscript_subject: subject,
+            custscript_template: template
+        }
+
+        reschedule = rescheduleScript(prev_inv_deploy, adhoc_inv_deploy, params);
+        nlapiLogExecution('AUDIT', 'Reschedule Return', reschedule);
+        if (reschedule == false) {
+            return false;
+        }
+
+        return true;
+    });
+
+}
+
+function getDate() {
+    var date = new Date();
+    if (date.getHours() > 6) {
+        date = nlapiAddDays(date, 1);
+    }
+    date = nlapiDateToString(date);
+
+    return date;
+}
