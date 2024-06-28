30c14d9eadc239329aba4116cb564bbf1f120d1f
ankith88 <ankith88@gmail.com>
2024-06-28 11:52:46 +1000

    CU-86cvu6key

--- mp_sl_merge_email.js
+++ mp_sl_merge_email.js
@@ -51,7 +51,7 @@ function main(request, response) {
             if (!isNullorEmpty(userID)) {
                 file.setEntity('employee', userID);
             }
-            
+
             if (!isNullorEmpty(contactID) && contactID != 0) {
                 if (contactID != 'null') {
                     file.setEntity('contact', contactID);
@@ -1920,6 +1920,24 @@ function main(request, response) {
 
                     emailHtml = emailHtml.replace(/nlemcontactfirstname/gi, firstname);
                     emailHtml = emailHtml.replace(/nlemcontactsupport/gi, portalOrientation);
+                }
+
+                //202406 - Premium - Lost - Send 1 - Intro
+                if (templateId == 458) {
+                    var recContact = nlapiLoadRecord('contact', contactID);
+
+                    var contactEmail = recContact.getFieldValue('email');
+                    var contactPhone = recContact.getFieldValue('phone');
+                    var firstname = recContact.getFieldValue('firstname');
+
+                    var reactivateAccountChangeStatusToHotLead = '<a class="mceButtonLink" href="https://mailplus.com.au/shipping-portal-orientation/?custinternalid=' + recId + '&custname=' + companyname + '&email=' + contactEmail + '&phone=' + contactPhone + '&firstname=' + firstname + '" style="background-color: #155370; border-radius: 50px;  color: #ffffff; display: block; font-family: "Helvetica Neue", Helvetica, Arial, Verdana, sans-serif; font-size: 16px; font-weight: bold; font-style: normal; padding: 16px 28px; text-decoration: none; min-width: 30px; text-align: center; direction: ltr; letter-spacing: 0px;" target="_blank" rel="noopener">REACTIVATE MY FREE ACCOUNT</a>'
+
+                    var optOutButton = '<a class="mceButtonLink" href="https://1048144.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=1896&deploy=1&compid=1048144&ns-at=AAEJ7tMQW4Gl6dYoqwmeMLNGC5DMzj_w0yktOoO0kv2PRzfvnPk/?custinternalid=' + recId + '&contactid=' + contactID + '" style="background-color: #ffffff; border-radius: 50px; border: 1px solid #000000; color: #000000; display: block; font-family: "Helvetica Neue", Helvetica, Arial, Verdana, sans-serif; font-size: 12px; font-weight: normal; font-style: normal; padding: 16px 28px; text-decoration: none; min-width: 30px; text-align: center; direction: ltr; letter-spacing: 0px;" target="_blank" rel="noopener">OPT OUT</a>'
+
+
+                    emailHtml = emailHtml.replace(/nlemcontactfirstname/gi, firstname);
+                    emailHtml = emailHtml.replace(/nlemreactivateaccountbutton/gi, reactivateAccountChangeStatusToHotLead);
+                    emailHtml = emailHtml.replace(/nlemoptoutbutton/gi, optOutButton);
                 }
 
 
